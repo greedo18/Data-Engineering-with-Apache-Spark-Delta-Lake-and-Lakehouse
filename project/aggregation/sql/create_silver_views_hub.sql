094c896c33af221883908008567469c564f41e53
greedo18 <pierre.carpentier.32@gmail.com>
2023-07-30 09:05:20 -0400

    Update create_silver_views_hub.sql

--- project/aggregation/sql/create_silver_views_hub.sql
+++ project/aggregation/sql/create_silver_views_hub.sql
@@ -6,13 +6,19 @@ CREATE PROCEDURE create_silver_views  @silverns varchar(50), @location varchar(2
 AS
 BEGIN
     DECLARE @sqlcmd nvarchar(MAX)
-    EXEC sp_executesql N'IF EXISTS ( SELECT * FROM sys.external_tables WHERE object_id = OBJECT_ID(''Customers'') )
+    
+    -- Drop the external table Customers if it exists
+    EXEC sp_executesql N'IF EXISTS (SELECT * FROM sys.external_tables WHERE object_id = OBJECT_ID(''Customers''))
                          DROP EXTERNAL TABLE Customers'
-    SET @sqlcmd = N'CREATE EXTERNAL TABLE CustomersInt  (Customer_id INTEGER, FirstName VARCHAR(255), LastName VARCHAR(255), DOB VARCHAR(255), updated_at DATETIME, SK VARCHAR(255)) 
-        WITH (LOCATION = ''' +  @silverns  + '/835837/Customers'',  data_source = trainingds, FILE_FORMAT = DeltaLakeFormat)';
-	EXEC sp_executesql @sqlcmd
 
+    -- Create the external table CustomersInt
+    SET @sqlcmd = N'CREATE EXTERNAL TABLE CustomersInt  (Customer_id INTEGER, FirstName VARCHAR(255), LastName VARCHAR(255), DOB VARCHAR(255), updated_at DATETIME, SK VARCHAR(255)) 
+        WITH (LOCATION = ''' +  @silverns  + '/835837/Customers'',  data_source = ''trainingds'', FILE_FORMAT = DeltaLakeFormat)';
+    
+    -- Execute the dynamic SQL statement
+    EXEC sp_executesql @sqlcmd;
 END;
 GO
+
 
 
